ee7abd066b7272cffe580783afc22466a6b62df3
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-12-25 18:22:09 +0100

    Added owner validation

--- app/src/main/java/io/gnosis/safe/ui/beggar/donate/SendFundsViewModel.kt
+++ app/src/main/java/io/gnosis/safe/ui/beggar/donate/SendFundsViewModel.kt
@@ -5,6 +5,7 @@ import io.gnosis.data.repositories.SafeRepository
 import io.gnosis.safe.R
 import io.gnosis.safe.ui.base.AppDispatchers
 import io.gnosis.safe.ui.base.BaseStateViewModel
+import io.gnosis.safe.ui.base.PublishViewModel
 import io.gnosis.safe.utils.OwnerCredentialsRepository
 import pm.gnosis.model.Solidity
 import javax.inject.Inject
@@ -28,12 +29,13 @@ class SendFundsViewModel
             val transactionHash = safeRepository.sendEthTxHash(
                 safe = activeSafe, receiver = receiver, value = amount.toBigInteger(), nonce = nonce
             )
-            updateState { SendFundsState(UserMessageWithArgs(R.string.retrieved_safe_nonce_on_chain, listOf(transactionHash))) }
+            updateState { SendFundsState(UserMessageWithArgs(R.string.retrieved_transaction_hash_on_chain, listOf(transactionHash))) }
         }
     }
 
-    private fun verifyOwner(safe: Solidity.Address) {
-        takeUnless { ownerCredentialsRepository.hasCredentials() && safe == ownerCredentialsRepository.retrieveCredentials()?.address }
+    private suspend fun verifyOwner(safe: Solidity.Address) {
+        val safeInfo = safeRepository.getSafeInfo(safe)
+        takeUnless { ownerCredentialsRepository.hasCredentials() && safeInfo.owners.contains(ownerCredentialsRepository.retrieveCredentials()?.address) }
             ?.run { throw CantTransfer }
     }
 }
